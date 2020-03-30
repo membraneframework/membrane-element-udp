2eec748b8476801455cfdbf9b698b6bd7ee1d8a5
Mateusz Front <mateusz.front@swmansion.com>
2020-03-30 09:55:53 +0200

    experimental: group packets

--- lib/membrane_element_udp/source.ex
+++ lib/membrane_element_udp/source.ex
@@ -4,8 +4,10 @@ defmodule Membrane.Element.UDP.Source do
   """
   use Membrane.Source
 
-  alias Membrane.Buffer
+  alias Membrane.{Buffer, Time}
   alias Membrane.Element.UDP.{CommonSocketBehaviour, Socket}
+
+  @timeout Time.milliseconds(100)
 
   def_options local_port_no: [
                 type: :integer,
@@ -29,6 +31,11 @@ defmodule Membrane.Element.UDP.Source do
                 description: """
                 Size of the receive buffer. Packages of size greater than this buffer will be truncated
                 """
+              ],
+              packets_per_buffer: [
+                type: :integer,
+                spec: pos_integer,
+                default: 1
               ]
 
   def_output_pad :output,
@@ -43,7 +50,19 @@ defmodule Membrane.Element.UDP.Source do
       sock_opts: [recbuf: opts.recv_buffer_size]
     }
 
-    {:ok, %{local_socket: socket}}
+    {:ok,
+     %{
+       local_socket: socket,
+       packets_per_buffer: opts.packets_per_buffer,
+       actions: [],
+       cache_size: 0,
+       latest_buffer_time: nil
+     }}
+  end
+
+  @impl true
+  def handle_prepared_to_playing(_ctx, state) do
+    {{:ok, start_timer: {:timer, @timeout}}, state}
   end
 
   @impl true
@@ -57,14 +76,40 @@ defmodule Membrane.Element.UDP.Source do
       |> Map.put(:udp_source_address, address)
       |> Map.put(:udp_source_port, port_no)
 
-    actions = [buffer: {:output, %Buffer{payload: payload, metadata: metadata}}]
+    actions = [buffer: {:output, %Buffer{payload: payload, metadata: metadata}}] ++ state.actions
+    cache_size = state.cache_size + 1
 
-    {{:ok, actions}, state}
+    if cache_size < state.packets_per_buffer do
+      state = %{
+        state
+        | actions: actions,
+          cache_size: cache_size,
+          latest_buffer_time: Time.monotonic_time()
+      }
+
+      {:ok, state}
+    else
+      {{:ok, Enum.reverse(actions)}, %{state | actions: [], cache_size: 0}}
+    end
   end
 
   @impl true
   def handle_other({:udp, _socket_handle, _address, _port_no, _payload}, _ctx, state) do
     {:ok, state}
+  end
+
+  @impl true
+  def handle_tick(:timer, _ctx, state) do
+    if state.cache_size == 0 or Time.monotonic_time() - state.latest_buffer_time < @timeout do
+      {:ok, state}
+    else
+      {{:ok, state.actions}, %{state | actions: [], cache_size: 0}}
+    end
+  end
+
+  @impl true
+  def handle_playing_to_prepared(_ctx, state) do
+    {{:ok, stop_timer: :timer}, state}
   end
 
   @impl true
